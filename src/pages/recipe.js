850c1645aa39a46b97ef9c5af5ea494fc37a0068
Toby Mundy <toby_mundy2@mymail.eku.edu>
2023-04-11 01:53:14 -0400

    Finished Gatsby Tutorial Part 2

--- src/pages/recipe.js
+++ src/pages/recipe.js
@@ -1,14 +1,44 @@
 import { graphql } from 'gatsby'
 import React from 'react'
 
-const pageTemplate = props =>{
+
+
+/*const pageTemplate = props =>{
     const data = {
         nodeFood: props.pageContext.data
     }
     console.log(data);
     return (
         <h2>{data.nodeFood?.title}</h2>
-    )}
+    )}*/
 
-export default pageTemplate
+    /*const pageTemplate = props =>{
+        const data = {
+            nodeFood: props.pageContext.data
+        }
+        console.log(data);
+        return (
+            <h2>{data}</h2>
+        )}*/
+
+       export default function Product({ pageContext })  {
+            const { data } = pageContext
+            console.log(data)
+            console.log("hello")
+            //const htmlString = data.recipeInstruction.processed
+            //const parser = new DOMParser();
+            //const html = parser.parseFromString(htmlString, 'text/html');
+            return (
+              <div>
+                <h2>{data.title}</h2>
+                
+                
+              </div>
+            )
+          }
+
+
+
+
+//export default pageTemplate
 
